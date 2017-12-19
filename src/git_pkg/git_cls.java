7c3532820324da09db6acb52e8fe4bc605322857
shary <sharyjay@gmail.com>
2017-12-19 13:11:37 +0530

    new chnages

--- src/git_pkg/git_cls.java
+++ src/git_pkg/git_cls.java
@@ -5,7 +5,7 @@ public class git_cls {
 	public static void main(String[] args) {
 		System.out.println("first git");
 		System.out.println("first git");
-		System.out.println("first git");
+		System.out.println("first1 git");
 		// TODO Auto-generated method stub
 		
 
